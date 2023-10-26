27025a4e7cf47694f3b72a9f0272b7699ee76081
Maxime Hell <maximehelldumons@gmail.com>
2023-10-26 14:19:20 +0200

    file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { RecapComponent } from './recap/recap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormulaireComponent,
+    RecapComponent
   ],
   imports: [
     BrowserModule
